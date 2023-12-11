f6f42eee5dc38a2ad228fbeef06385e8827f2240
liyou-chen <liyou1202@gmail.com>
2023-12-11 15:44:44 +0900

    Initial Commit

---
+++ isTimeLegal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  isTimeLegal
+//
+//  Created by 陳禮佑 on 2023-12-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
